0796bce5ec8cf3dceb2bfa6ebe39d9407a9e633c
Anaid-Ariwany <anaidariwany@gmail.com>
2026-02-16 15:55:18 +0300

    ToDo List Project: Completed the project

--- JavaScript/Projects/ToDo List/todoList.js
+++ JavaScript/Projects/ToDo List/todoList.js
@@ -1,1 +1,28 @@
-console.log("hello");
+let userInput = prompt("What do you want to do? NEW, LIST, or DELETE?");
+let arrTask = [];
+
+while (userInput.toUpperCase() !== 'QUIT') {
+    if (userInput.toUpperCase() === 'NEW') {
+        input = prompt("Enter a task to add?");
+        arrTask.push(input.toUpperCase());
+        console.log(`${input.toUpperCase()} added to the list`);
+    }
+
+    else if (userInput.toUpperCase() === 'LIST') {
+        for (let i = 0; i < arrTask.length; i++) {
+            console.log(i, arrTask[i]);
+        }
+    }
+
+    else if (userInput.toUpperCase() === 'DELETE') {
+        input = parseInt(prompt("Enter index of item to delete?"));
+        arrTask.splice(input, 1);
+        console.log('Task deleted!')
+        console.log(arrTask);
+    }
+
+    userInput = prompt("What do you want to do? NEW, LIST, or DELETE?");
+
+}
+console.log("You quit the program");
+
